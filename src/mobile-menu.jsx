b4de172a1cb62db06dce52554b148a7dccc8f712
Ozone <mohammaddr653@gmail.com>
2024-06-11 14:03:55 +0330

    added home-main-trust

--- src/mobile-menu.jsx
+++ src/mobile-menu.jsx
@@ -13,16 +13,14 @@ const MobileMenu = (props) => {
     return ( 
         <div id='mobile-menu' className={mobMenuContext.mobIconClicked ? "mobile-menu-show" : null}>
             <div className="mobile-menu-container container-fluid w-100">
-                <div className="row">
-                    <div className="col close-button" >
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16"
-                            onClick={function(){
-                                mobMenuContext.setMobIconClicked(false);
-                            }}
-                        >
-                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                        </svg>
-                    </div>
+                <div className="close-button" >
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16"
+                        onClick={function(){
+                            mobMenuContext.setMobIconClicked(false);
+                        }}
+                    >
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                    </svg>
                 </div>
                 <div className='row'>
                     <div className="col mob-search-container">
