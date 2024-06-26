9c0515b48cbf22ee84b6eaed81ba4c94f16035cd
Ozone <mohammaddr653@gmail.com>
2024-06-27 00:54:34 +0330

    added main slider

---
+++ src/home-main-slider.jsx
@@ -0,0 +1,32 @@
+import './css/fonts.css';
+import './css/home-main-slider.css';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+
+
+const HomeMainSlider = () => {
+
+    return(
+        <div id='home-main-slider-container'>
+            <div className='container'>
+                <div className="row p-0">
+                    <Carousel className='p-0 slider-container'>
+                      <Carousel.Item interval={1000}>
+                        <img src='images/dr1.jpg'/>
+                      </Carousel.Item>
+                      <Carousel.Item interval={500}>
+                        <img src='images/dr2.jpg' />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img src='images/dr3.jpg' />
+                      </Carousel.Item>
+                    </Carousel>
+                </div>
+            </div>
+        </div>
+    );
+} 
+export default HomeMainSlider;
--- src/home-main.jsx
+++ src/home-main.jsx
@@ -6,12 +6,14 @@ import HomeMainDrSlider from './home-main-dr-slider';
 import HomeMainDoctorsArc from './home-main-doctors-arc';
 import HomeMainHowItWorks from './home-main-how-it-works';
 import HomeMainTapeshFacilities from './home-main-tapesh-facilities';
+import HomeMainSlider from './home-main-slider';
 const HomeMain = () => {
     return ( 
         <main>
             <div id="main-container" className='container-fluid p-0 w-100'>
                 <HomeMainBanner></HomeMainBanner>
                 <HomeMainTrust></HomeMainTrust>
+                <HomeMainSlider></HomeMainSlider>
                 <HomeMainDrSlider></HomeMainDrSlider>
                 <HomeMainDoctorsArc></HomeMainDoctorsArc>
                 <HomeMainHowItWorks></HomeMainHowItWorks>
