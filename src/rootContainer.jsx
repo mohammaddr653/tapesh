4b415da1e11c379c9fcae8b0df7f165d9f06a7b5
Ozone <mohammaddr653@gmail.com>
2024-07-05 11:16:05 +0330

    footer added

--- src/rootContainer.jsx
+++ src/rootContainer.jsx
@@ -1,12 +1,14 @@
 import Header from "./header";
 import HomeMain from "./home-main";
 import './css/rootContainer.css';
+import Footer from "./footer";
 
 const RootContainer = () => {
     return (
         <div id="root-container">
             <Header></Header>
             <HomeMain></HomeMain>
+            <Footer></Footer>
         </div>
     );
 }
