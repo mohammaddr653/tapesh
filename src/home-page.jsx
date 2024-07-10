779897b0e080ebe9a285ffbe03d0418f06bfbb26
Ozone <mohammaddr653@gmail.com>
2024-07-10 12:42:32 +0330

    some edits

--- src/home-page.jsx
+++ src/home-page.jsx
@@ -2,8 +2,16 @@ import Header from "./header";
 import HomeMain from "./home-main";
 import './css/home-page.css';
 import Footer from "./footer";
+import { useEffect } from "react";
 
 const HomePage = () => {
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+    },[]);
     return (
         <div id="home-page">
             <Header></Header>
