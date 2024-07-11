087ccf130e3622a60d03db1f555255e7d4afd937
Ozone <mohammaddr653@gmail.com>
2024-07-11 21:05:11 +0330

    doc single ready to modify

--- src/doc-single-main.jsx
+++ src/doc-single-main.jsx
@@ -10,9 +10,13 @@ import FilteredContext from './context/filtered';
 
 const DocSingleMain = () => {
     return ( 
-        <div>
-            jlkj
-        </div>
+        <main>
+            <div id='doc-single-main-container' className='container-fluid w-100 bg-danger'>
+                <div className='container p-0 bg-dark'>
+                    <div className="row w-100 m-0 p-0 bg-success">sdsd</div>
+                </div>
+            </div>
+        </main>
     );
 }
  
