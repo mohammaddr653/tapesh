dd9724bfae010752da8e8b6c6cafd11f6bd5b674
Ozone <mohammaddr653@gmail.com>
2024-07-18 03:34:29 +0330

    cart completed

--- src/user-cart-main.jsx
+++ src/user-cart-main.jsx
@@ -12,118 +12,45 @@ import DoctorsInfoContext from './context/doctorsInfo';
 const UserCartMain = () => {
     let cartContext=useContext(CartContext);
     let doctorsInfoContext=useContext(DoctorsInfoContext);
-    let[cartArray,setCartArray]=useState([]);
-    // useEffect(()=>{
-    //     for(let id of cartContext.cart){
-    //         for(let doctor of doctorsInfoContext.doctors){
-    //             if(doctor.id===id){
-    //                 console.log(doctor.name);
-    //                 let a=cartArray;
-    //                 a.push(doctor);
-    //                 setCartArray([a]);
-    //             }
-    //         }
-    //     }
-    // },[cartContext,doctorsInfoContext])
-
-    // function checkRepeat(){
-    //     let listLi=document.getElementsByClassName("list-li");
-    //     let listSpan=document.getElementsByClassName("list-span");
-    //     for(let n=0;n<listSpan.length;n++){
-    //         console.log("n is "+ n);
-    //         for(let x=0;x<listSpan.length;x++){
-    //             console.log("x is "+ x);
-    //             console.log(listSpan[n] , listSpan[x]);
-    //             if(!(x===n) && listSpan[n].innerHTML===listSpan[x].innerHTML){
-    //                 console.log("n span is "+listSpan[n].innerHTML);
-    //                 console.log("x span is "+listSpan[x].innerHTML);
-    //                 let nRepeated=listLi[n]
-    //                 let xRepeated=listLi[x];
-    //                 if(!(nRepeated.style.display==="none")){
-    //                     xRepeated.style.display="none";
-    //                     console.log(cartContext.cart.indexOf(listSpan[x].innerHTML));
-    //                 }
-    //             }else{
-    //                 console.log("not match");
-    //             }
-    //         }
-    //     }
-    // }
-
-    // function enableButtons(){
-    //     alert("published");
-    //     if(document.getElementsByClassName("delete")[3]){
-    //         let listLi=document.getElementsByClassName("list-li");
-    //         let listInput=document.getElementsByClassName("list-span");
-    //         let increaseButt=document.getElementsByClassName("increase");
-    //         let counter=document.getElementsByClassName("counter");
-    //         let decreaseButt=document.getElementsByClassName("decrease");
-    //         let deleteButt=document.getElementsByClassName("delete");
-    //         console.log(deleteButt);
-
-    //         console.log("listInput[1] value is : ")
-    //         console.log(listInput[1]);
-    //         deleteButt[1].addEventListener("click",function (event){
-    //             event.preventDefault();
-    //             jj();
-    //         })
-    //         function jj(){
-    //             function myDisplayer(some) {
-    //                 cartContext.setCart(some);
-    //                 console.log("cartContext.setCart(x) : ");
-    //                 console.log("delete end");    
-    //             }    
-    //             let myPromise = new Promise(function(myResolve, myReject) {
-    //                 console.log("delete start");
-    //                 console.log("input is : ")
-    //                 console.log(listInput[1]);
-    //                 console.log("input value is : ")
-    //                 console.log(listInput[1].defaultValue);
-    //                 console.log("cartContext is : ");
-    //                 console.log(cartContext.cart);
-    //                 let a=cartContext.cart;
-    //                 console.log("a is : ");
-    //                 console.log(a);
-    //                 let x = a.filter(e=>!(e.id === listInput[1].defaultValue));
-    //                 console.log("x is : ");
-    //                 console.log(x);
-                
-                    
-    //                 if (x) {
-    //                   myResolve(x);
-    //                 } else {
-    //                   myReject(null);
-    //                 }
-    //             });
-    //             myPromise.then(
-    //                 function(value) {myDisplayer(value);},
-    //                 function(error) {myDisplayer(error);}
-    //               );
-          
-    //         }  
-    //     }    
-
-    //     for(let n=0;n<listLi.length;n++){
-    //         increaseButt[n].addEventListener("click",function(){
-    //             console.log(counter[n]);
-    //             counter[n].innerHTML=parseInt(counter[n].innerHTML)+1;
-
-    //         });
-    //         decreaseButt[n].addEventListener("click",function(){
-    //             console.log(counter[n]);
-    //             if(parseInt(counter[n].innerHTML)>0){
-    //                 counter[n].innerHTML=parseInt(counter[n].innerHTML)-1;
-    //             }
-    //         });
-    //     }
-
-    // }
-
+    let[totalPrice,setTotalPrice]=useState(0);
     useEffect(()=>{
-        console.log(cartContext.cart)
+        console.log(cartContext.cart);
+        let priceResult=0;
+        let x=false;
+        if(!(cartContext.cart.length===0)){
+            for(let item of cartContext.cart){
+                let count=parseInt(item.count);
+                let price=parseInt(item.price);
+                console.log(count);
+                console.log(price);
+                let cross=count*price;
+                console.log(cross);
+                priceResult +=cross;
+                console.log(priceResult);
+            }    
+        }
+        x=true;
+        if(x===true){
+            setTotalPrice(()=>{
+                let t=priceResult;
+                return t;
+            })
+        }
+        
     },[cartContext.cart])
-    function jj(e){
-        let li=e.target.parentElement.parentElement;
+
+    function listDelete(e,handler){
+        let li;
+        if(handler==="decrease"){
+            console.log("decrease");
+            li=e.target.parentElement.parentElement;
+        }
+        if(handler==="delete"){
+            console.log("delete");
+            li=e.target.parentElement.parentElement.parentElement;
+        }
+
+        // let li=e.target.parentElement.parentElement.parentElement;
         console.log("li is : ")
         console.log(li);
         // let span=li.children[0];
@@ -152,7 +79,7 @@ const UserCartMain = () => {
     return(
         <main>
             <div id="user-cart-main-container" className="container-fluid w-100">
-                <div className="container p-0">
+                <div className="container">
                     <div className='row user-cart-title w-100 p-0'>
                         <h3>نوبت های شما</h3>
                         <hr />
@@ -161,11 +88,11 @@ const UserCartMain = () => {
                         <div className="col bg-light reserve-list border p-3">
                             <h4>لیست نوبت ها</h4>
                             <ul>
-                                {cartContext.cart ? cartContext.cart.map((item,index)=>{
+                                {!(cartContext.cart.length===0) ? cartContext.cart.map((item,index)=>{
                                     return(
                                         <li key={index} className='border list-li'>
                                             <input className='list-span d-none' defaultValue={item.id}></input>
-                                            <div className='item-img'>
+                                            <div className=' item-img'>
                                                 <img src={item.img} alt="#" />
                                                 <h4>{item.name}</h4>
                                             </div>
@@ -179,8 +106,8 @@ const UserCartMain = () => {
                                                     let a=cartContext.cart;
                                                     for(let object of a){
                                                         let objectVal=parseInt(object.id);
-                                                        let spanVal=parseInt(span.value);
-                                            
+                                                        let spanVal=parseInt(span.defaultValue);
+
                                                         if(objectVal===spanVal){
                                             
                                                             let index = a.indexOf(object);
@@ -209,7 +136,7 @@ const UserCartMain = () => {
                                                         let a=cartContext.cart;
                                                         for(let object of a){
                                                             let objectVal=parseInt(object.id);
-                                                            let spanVal=parseInt(span.value);
+                                                            let spanVal=parseInt(span.defaultValue);
                                                 
                                                             if(objectVal===spanVal){
                                                 
@@ -227,26 +154,31 @@ const UserCartMain = () => {
                                                             
                                                         }                                                
                                                     }else{
-                                                        jj(e);
+                                                        listDelete(e,"decrease");
                                                     }
                                                 }}>-</button>
                                                 <button className='bg-danger text-light delete' onClick={function(e){
-                                                    jj(e);
+                                                    listDelete(e,"delete");
                                                 }}>
-                                                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                                                    </svg> */}
-                                                    *
+                                                    <img src="images/trash3.svg" className='bg-danger rounded' alt="" />
                                                 </button>
                                             </div>
                                         </li>
                                     )
-                                }) : null}
+                                }) : "سبد خرید شما خالی است"}
                             </ul>
                         </div>
-                        <div className="col bg-light border p-3">
+                        <div className="col bg-light border finish-reserve p-3">
                             <h4>نهایی کردن رزرو</h4>
-
+                            <div>
+                                {
+                                    totalPrice
+                                }
+                                <span>
+                                    تومان
+                                </span>
+                            </div>
+                            <button className='bg-primary btn text-light'>پرداخت</button>
                         </div>
                     </div>
 
